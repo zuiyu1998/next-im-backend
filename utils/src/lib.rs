8fd3b44e857c6938957452790bc99e1c3f2f46ca
zuiyu1998 <1542844298@qq.com>
2024-05-24 09:48:25 +0800

    feat: 服务获取器

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -3,4 +3,44 @@ pub mod service_register_center;
 
 pub mod error;
 
+use abi::{tonic::async_trait, tracing::warn};
+use service_discovery::ServiceFetcher;
+use service_register_center::ServiceRegister;
+use std::{collections::HashSet, net::SocketAddr, sync::Arc};
+
 pub use error::*;
+
+pub struct ServiceResolver {
+    service_name: String,
+    service_center: Arc<dyn ServiceRegister>,
+}
+
+#[async_trait]
+impl ServiceFetcher for ServiceResolver {
+    async fn fetch(&self) -> Result<HashSet<SocketAddr>> {
+        let map = self
+            .service_center
+            .filter_by_name(&self.service_name)
+            .await?;
+        let x = map
+            .values()
+            .filter_map(|v| match format!("{}:{}", v.address, v.port).parse() {
+                Ok(s) => Some(s),
+                Err(e) => {
+                    warn!("parse address error:{}", e);
+                    None
+                }
+            })
+            .collect();
+        Ok(x)
+    }
+}
+
+impl ServiceResolver {
+    pub fn new(service_center: Arc<dyn ServiceRegister>, service_name: String) -> Self {
+        Self {
+            service_name,
+            service_center,
+        }
+    }
+}
