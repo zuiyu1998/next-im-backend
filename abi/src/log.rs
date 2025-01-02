aa87fbf3078ff4238f3127b8dfc59d6623bbd407
zuiyu1998 <1542844298@qq.com>
2025-01-02 21:30:02 +0800

    fix: 修复日志

--- abi/src/log.rs
+++ abi/src/log.rs
@@ -3,14 +3,12 @@ use std::error::Error;
 use crate::config::Config;
 use tracing_subscriber::{
     filter::{FromEnvError, ParseError},
+    fmt,
     prelude::*,
-    registry::Registry,
     EnvFilter,
 };
 
 pub fn tracing_subscriber_init(config: &Config) {
-    let subscriber = Registry::default();
-
     let default_filter = { format!("{},{}", config.log.level.level(), config.log.filter) };
     let filter_layer = EnvFilter::try_from_default_env()
         .or_else(|from_env_error| {
@@ -25,5 +23,8 @@ pub fn tracing_subscriber_init(config: &Config) {
         })
         .unwrap();
 
-    subscriber.with(filter_layer).init();
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(filter_layer)
+        .init()
 }
